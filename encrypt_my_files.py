e9ef818ac766d8f4b268c3a4e04dee6457ceb664
Nameless <64658116+roflsec1@users.noreply.github.com>
2022-08-27 07:56:30 +0000

    Added password protection for PDFs
    Takes roughly 10 seconds

---
+++ encrypt_my_files.py
@@ -0,0 +1,17 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import subprocess
+import pyAesCrypt
+import re
+
+input_file = "sample.pdf"
+with open(input_file, "rb") as pdf_file:
+    reader = PdfFileReader(input_file, strict=False)
+
+writer = PdfFileWriter()
+writer.appendPagesFromReader(reader)
+writer.encrypt("0000")
+encrypted_file_name = "{}_encrypted.pdf".format(re.split("[.]", input_file)[0])
+with open(encrypted_file_name, 'wb') as f:
+    writer.write(f)
+
+
